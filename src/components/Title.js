5af6d00a6578d28bc34216237af4805ef1e67495
ironpup <christoph.ding@gmail.com>
2017-01-18 14:56:27 -0500

    Add constants to Title component to make this component more easy to configure in the future.

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,15 @@
 "use strict";
 import React from 'react';
 
+const title = 'I am the title of this app';
+const description = 'I describe what this app does';
+
 class Title extends React.Component{
 	render() {
 		return (
 			<div>
-				<h1> Title </h1>
-				<p> description </p>
+				<h1> {title} </h1>
+				<p>  {description} </p>
 			</div>
 		)
 	}
