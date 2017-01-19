931af580274f13cf21660a9412ef8f72ce119c70
ironpup <christoph.ding@gmail.com>
2017-01-19 11:09:42 -0500

    Add groupBy function which creates an object with keys for the levels of a category in the data.

--- src/getData.js
+++ src/getData.js
@@ -1,12 +1,36 @@
 console.log('fetching the data');
 
-var APIEndpoint = 'https://restcountries.eu/rest/v1/all';
+const worldDataURL = 'https://restcountries.eu/rest/v1/all';
 
-fetch(APIEndpoint)
-.then((res) => {
-	console.log('got the res');
-	res.json()
-		 .then((data) => {
-		 		console.log(data);
-			})
-})
+const getDataFromAPI = (url) => {
+	fetch(url)
+	.then((res) => {
+		console.log('got the res');
+		res.json()
+			 .then((data) => {
+			 		console.log(data);
+			 		groupBy(data, 'region');			 		
+				})
+	})
+}
+
+const groupBy = (data, key) => {
+	const groupedData = {};
+
+	for (var datum of data) {
+		var keyValue = datum[key];
+		var newGroupValue = !groupedData.hasOwnProperty(keyValue);
+
+		// add datum to appropriate group level
+		// all levels in the data are captured
+		if (newGroupValue) {
+			groupedData[keyValue] = [];
+		} 
+		var grouping = groupedData[keyValue];
+		grouping.push(datum);
+	}
+
+	console.log(groupedData);
+}
+
+getDataFromAPI(worldDataURL);
