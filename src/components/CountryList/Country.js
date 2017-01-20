13f5d0cc47804a9f7582e411e2cb7f0123bf2aa9
ironpup <christoph.ding@gmail.com>
2017-01-20 14:32:05 -0500

    Connect Grouping and single Country components.

--- src/components/CountryList/Country.js
+++ src/components/CountryList/Country.js
@@ -4,7 +4,7 @@ import React from 'react';
 class Country extends React.Component{
   render() {
     return (
-      <h3> I'm a country </h3>
+      <h4> {this.props.countryData.name} </h4>
     );
   }
 }
--- src/components/CountryList/Grouping.js
+++ src/components/CountryList/Grouping.js
@@ -6,8 +6,14 @@ class Grouping extends React.Component{
   render() {
     return (
       <div>
-        <h2> {this.props.level} </h2>
-        <h3> {this.props.countryList[0].name} </h3>
+        <h2> {this.props.level} </h2>        
+        {
+          this.props.countryList.map((country) => {
+            return (
+              <Country countryData={country}/>
+            )
+          })
+        }
       </div>
     )
   };
