73bcec203b38af9d9c86d578c765bf50e8f6651f
ironpup <christoph.ding@gmail.com>
2017-01-29 16:25:10 -0500

    Add some test field headers to inject to app.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import './styles/main.css';
 import WorldDataApp from './components/App.js';
 
 const keyMapping = {'': 'not available'}
-// const relevantFields = [{original: , translation:}];
+const relevantFields = {'name': 'Name', 'alpha2code': 'Code', 'capital:' 'Capital', 'region': 'Region', 'subregion': 'Subregion', 'population': 'Population', 'area': 'Area'}
 
 render(<WorldDataApp keyMapping={keyMapping}/>, document.getElementById('root'));
