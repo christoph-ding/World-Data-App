0ced1f7b7db2d216539f8b9c5b137b73f0329a6f
ironpup <christoph.ding@gmail.com>
2017-01-30 11:23:27 -0500

    Refactor how the elements in the table are orgaznied.

--- src/components/CountryList/CountryList.js
+++ src/components/CountryList/CountryList.js
@@ -1,24 +1,28 @@
 "use strict";
 import React from 'react';
 import Grouping from './Grouping.js';
+import FieldNames from './FieldNames.js';
 
 class CountryList extends React.Component{
 
   render() {
     // a level is a possible answer for the group chosen
     const levels = Object.keys(this.props.countryData); 
-    return (
-      <div>
-      {
-        levels.map((level) => {          
-          let levelCountries = this.props.countryData[level];        
-          return (
-            <Grouping key={level} level={level} countryList={levelCountries}/>
-          );
-        })
-      }
-      </div>
-    )
+      return (
+        <table>
+          <tbody>
+            <FieldNames fields={this.props.fields}/>
+            {
+              levels.map((level) => {          
+                let levelCountries = this.props.countryData[level];        
+                return (
+                  <Grouping key={level} level={level} countryList={levelCountries}/>
+                );
+              })
+            }
+          </tbody>
+        </table>
+      )
   }
 }
 
--- src/components/CountryList/Grouping.js
+++ src/components/CountryList/Grouping.js
@@ -6,20 +6,18 @@ class Grouping extends React.Component{
 
   render() {
     return (
-      <div>
-        <h2>{this.props.level}</h2>
-          <table>
-          <tbody>            
-            {
-              this.props.countryList.map((country) => {
-                return (
-                  <Country key={country.name} id={'name'} action={this.expandList} countryData={country}/>
-                )
-              })
-            }            
-          </tbody>
-        </table>
-      </div>
+      <tbody> 
+          <tr>
+            <td> {this.props.level} </td>
+          {
+            this.props.countryList.map((country) => {
+              return (
+                <Country key={country.Name} id={'name'} action={this.expandList} countryData={country}/>
+              )
+            })
+          }
+          </tr>
+      </tbody>
     )
   };
 }
