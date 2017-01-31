80a83093c5947bc1f9bea7b3f18e81a8dca19e37
ironpup <christoph.ding@gmail.com>
2017-01-31 11:18:29 -0500

    Styling for the elements in the title and description box.

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const title = 'World Data Viewer';
-const description = 'This is an app for ';
+const description = 'Glossier cred keffiyeh kinfolk. Etsy 8-bit seitan 3 wolf moon, kombucha street art master cleanse yr lo-fi roof party vinyl chillwave meggings. Salvia 3 wolf moon master cleanse enamel pin scenester post-ironic, authentic humblebrag coloring book green juice gluten-free put a bird on it 90s.  Lumbersexual fam chillwave live-edge swag, sustainable copper mug. Hot chicken distillery bushwick, master cleanse air plant forage marfa austin migas. Affogato actually portland, offal disrupt swag readymade pabst etsy man bun.';
 
 class Title extends React.Component{
   render() {
