1518cf0fffdf105b7786d9983241c97d4349009a
ironpup <christoph.ding@gmail.com>
2017-02-01 15:51:30 -0500

    Refactor WorldDataApp tests to use beforeEach hook.

--- test/app.spec.js
+++ test/app.spec.js
@@ -5,16 +5,25 @@ import { shallow, mount } from 'enzyme';
 import WorldDataApp from '../src/components/App.js';
 import Title from '../src/components/Title.js';
 import FilterDataForm from '../src/components/FilterDataForm/FilterDataForm.js';
-import CountryList from '../src/components/CountryList/CountryList';
+import CountryList from '../src/components/CountryList/CountryList.js';
 
-describe('test', () => {
-  it('should contain Title, FilterDataForm and CountryList components', ()=> {
-    const wrapper = shallow(<WorldDataApp/>);
-    expect(wrapper.containsAllMatchingElements([
-      <Title/>,
-      <FilterDataForm/>,
-      <CountryList/>
-    ])).to.equal(true);
+describe('App elements', () => {
+  let wrapper;
+
+  beforeEach( () => {
+    wrapper = shallow(<WorldDataApp/>);
   });
 
+  it('should contain a Title component', () => {
+    expect(wrapper.contains(<Title/>));
+  })
+
+  it('should contain a FilterDataForm component', () => {
+    expect(wrapper.contains(<FilterDataForm/>));
+  })
+
+  it('should contain a CountryList component', () => {
+    expect(wrapper.contains(<CountryList/>));
+  })
+  
 }); 
