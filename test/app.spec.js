a8ec728b9ed9f0e3ac14f97e4726b69b9511e2b8
ironpup <christoph.ding@gmail.com>
2017-02-01 17:09:48 -0500

    Write tests for WorldDataApps initial state.

--- test/app.spec.js
+++ test/app.spec.js
@@ -28,3 +28,27 @@ describe('App elements', () => {
   })
   
 }); 
+
+describe('App initial state and data', () => {
+  // set up the component
+  let wrapper;
+  beforeEach( () => {
+    wrapper = shallow(<WorldDataApp/>);
+  });
+
+  it('should start with states that hold world data', () => {
+    expect(wrapper.state(['formattedRawData'])).to.eql([]);
+    expect(wrapper.state(['groupedData'])).to.eql({});
+    expect(wrapper.state(['dataFields'])).to.eql([]);    
+  });
+
+  it('should start with states that hold the user chosen filter parameters', () => {
+    expect(wrapper.state(['selectGrouping'])).to.eql(undefined);
+    expect(wrapper.state(['selectedSorting'])).to.eql(undefined);
+    expect(wrapper.state(['selectedFilterField'])).to.eql(undefined);
+    expect(wrapper.state(['selectedOperator'])).to.eql(undefined);
+    expect(wrapper.state(['filterThreshold'])).to.eql('');
+
+  })
+
+})
