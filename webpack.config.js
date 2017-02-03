14acdd7d94c5cb9f51b092618e13a38660fa1011
ironpup <christoph.ding@gmail.com>
2017-02-03 15:15:34 -0500

    Fix some type in the webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var BUILD_DIR = path.resolve(__dirname, 'dist');
 var APP_DIR = path.resolve(__dirname, 'src');
 
 var config = {
-  entry: [APP_DIR + '/index.js'],
+  entry: [path.resolve(APP_DIR + '/index.js')],
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
